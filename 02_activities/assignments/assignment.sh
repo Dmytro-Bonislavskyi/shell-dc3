e92e6afde07689b5bca6707a3c4fd4d973c36fbf
Dmytro-Bonislavskyi <Dmytro.Bonislavskyi@gmail.com>
2026-02-18 14:15:07 -0500

    Fulfilled assignment 1

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,23 +28,30 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+mkdir data
+git status
+# 2. Move the ./rawdata directory to ./data/raw
+mv rawdata data/raw
 
-# 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
+# 4. List the contents of the ./data/raw directory
+ls data/raw
 
-# 3. List the contents of the ./data/raw directory
+# 5. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir -p data/processed/{server_logs,user_logs,event_logs}
 
-# 4. Create the directory ./data/processed, 
-#    then create the following sub-directories within it: server_logs, user_logs, and event_logs
+# 6. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cp data/raw/*server*.log data/processed/server_logs/
 
-# 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
-# 6. Repeat the above step for user logs and event logs
-
-# 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+# 7. Repeat the above step for user logs and event logs
+cp data/raw/*user*.log data/processed/user_logs/
+cp data/raw/*event*.log data/processed/event_logs/
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+find data/processed -type f > data/inventory.txt
 
-
-###########################################
+# 9. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm data/raw/*ipaddr*
+rm data/processed/user_logs/*ipaddr*
+#############################################
 
 echo "Project setup is complete!"
